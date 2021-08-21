9fc77073f9312da4e16e6804fd7a065fefa62a21
Leonardo Mendonça <leonardonm1987@gmail.com>
2021-08-21 16:54:39 -0300

    Nova atualização.

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-echo "Essa linha foi adicionada direto no github";
 
-echo "<h2><s>Testando git!</h2>";
-
-echo "<h1>Este é um segundo commit!</h1></s>";
-
-
+echo "Nova alteração...";
 
 ?>
