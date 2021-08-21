64bba7b40b1557673bdc7d2c40dd44c091d236dd
Leonardo Mendonça <leonardonm1987@gmail.com>
2021-08-21 17:20:04 -0300

    Várias alterações no layout

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Learning GIT</title>
+</head>
+<body>
 
-echo "Nova alteração...";
+<main>
 
-?>
+	<section>
+
+		<h1>Learning About Something</h1>
+
+		<?php
+
+		echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum esse dolores maiores ipsam suscipit nemo atque dolorem minima necessitatibus excepturi asperiores vel minus, veniam aut at ullam amet quae eligendi? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci pariatur ea quibusdam at asperiores. Esse incidunt dicta, labore, adipisci ipsa illo ex, qui inventore id nobis neque! Aut, aliquid placeat..</p>";
+
+		echo "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia officiis nostrum cupiditate incidunt repellat quas nam quam commodi, fugiat laborum cum temporibus a molestiae animi et architecto praesentium quidem recusandae.</p>";
+
+		echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptas aut distinctio hic amet doloremque quos ad, veritatis repudiandae, quia tempora ex corporis deleniti excepturi. Sit quia molestiae tempora ullam.</p>";
+
+		?>
+
+	</section>
+</main>
+
+</body>
+</html>
+
+
+
+
+
